cb817a2e441d0d44a9c09b1fc053861e7d2f60d4
omar sawy <omarsawy51@gmail.com>
2024-09-20 16:31:19 +0300

    move users to common classes folder

--- SciMagazine.Core/Common/Classes/User.cs
+++ SciMagazine.Core/Common/Classes/User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SciMagazine.Core.Common
+namespace SciMagazine.Core.Common.Classes
 {
     public abstract class User
     {
