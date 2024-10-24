c2f8642bbd4f04e9c145fafaf190a40e488142d2
omar sawy <omarsawy51@gmail.com>
2024-10-25 00:02:53 +0300

    add type safety and change delete method return type

--- SciMagazine.Application/Common/Interfaces/IBaseRepository.cs
+++ SciMagazine.Application/Common/Interfaces/IBaseRepository.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace SciMagazine.Core.Common.Interfaces
 {
-    public interface IBaseRepository<T>
+    public interface IBaseRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(int id);
+        Task<bool> DeleteAsync(int id);
     }
 }
