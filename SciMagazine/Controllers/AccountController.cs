411397fd02a7f06da5fb5896d74a588f887704d4
omar sawy <omarsawy51@gmail.com>
2024-10-24 23:46:43 +0300

    add Account Controller

---
+++ SciMagazine/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SciMagazine.Application.DTOs;
+using SciMagazine.Application.Interfaces.IServices;
+
+namespace SciMagazine.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly IEmailServices _emailServices;
+        public AccountController(IEmailServices emailServices)
+        {
+            _emailServices = emailServices;
+        }
+
+        [HttpPost("send-register-request")]
+        public async Task<IActionResult> SendRegisterRequest(RegisterRequestEmail request)
+        {
+            var result = await _emailServices.SendApprovalEmailToAdmin(request);
+            return Ok(result);
+        }
+    }
+}
