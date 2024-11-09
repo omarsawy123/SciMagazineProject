1a6427a3ada858c905a25301169ab8356aaf6922
omar sawy <omarsawy51@gmail.com>
2024-11-09 21:47:02 +0200

    Add RegisterAttachments

---
+++ SciMagazine.Core/Interfaces/IFileAttachment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciMagazine.Core.Interfaces
+{
+    public interface IFileAttachment
+    {
+        string Name { get; }
+        string Description { get; }
+        string FilePath {  get; }
+    }
+}
